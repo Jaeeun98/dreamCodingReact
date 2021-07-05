5d5f1582f91e08adb9f69b6bdbdd7074bd71152c
Jaeeun98 <wodms4525@naver.com>
2021-07-05 15:29:06 +0900

    Title : Add
    youtube api 받아와서 인기있는 동영상 순서대로 보여주기(스스로) add

--- youtube_project/src/app.jsx
+++ youtube_project/src/app.jsx
@@ -1,11 +1,12 @@
 import './app.css';
 import Search from './components/search'
+import Section from './components/section';
 
 function App() {
   return (
     <div className="app">
       <Search />
-
+      <Section />
     </div>
   );
 }
