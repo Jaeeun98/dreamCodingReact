103ed35cf83878cad74f2b4faeccd72a46493867
Jaeeun98 <wodms4525@naver.com>
2021-07-05 16:39:36 +0900

    Title : Change
    youtube api 받아와서 인기있는 동영상 순서대로 보여주기 솔루션 Change

--- youtube_project/src/app.jsx
+++ youtube_project/src/app.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react';
 import './app.css';
-import Search from './components/search'
-import Section from './components/section';
+import VideoList from './components/videoList/videoList';
+import Search from './components/search';
 
 function App() {
+  const [videos, setVideo] = useState([]);
+
+  useEffect(() => {
+    const key = 'AIzaSyDNGkQUxzgaQ5uAPFhyn7wz4rRyBDbVp7I'
+      
+    fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=${key}`, { method: 'GET' })
+        .then(response => response.json())
+        .then(result =>  setVideo(result.items))
+        .catch(error => console.log('error', error));
+            
+  }, [])
+
   return (
     <div className="app">
       <Search />
-      <Section />
+      <VideoList 
+        videos={videos}
+      />
     </div>
   );
 }
---
+++ youtube_project/src/components/videoList/videoList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import VideoItem from '../videoItem/videoItem';
+import styles from './videoList.module.css'
+
+const VideoList = props => (
+    <ul className={styles.videos}>
+        {props.videos.map(video => (
+            <VideoItem 
+                key={video.id}
+                video={video}
+            />
+        ))}
+    </ul>
+)
+    
+
+export default VideoList;
