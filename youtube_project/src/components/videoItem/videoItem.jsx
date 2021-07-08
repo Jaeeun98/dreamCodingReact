0fd16fadf271cd88bf3c4370d677c1772892f9df
Jaeeun98 <wodms4525@naver.com>
2021-07-08 17:51:02 +0900

    Title : Change
    unescape 사용, title change

--- youtube_project/src/components/videoItem/videoItem.jsx
+++ youtube_project/src/components/videoItem/videoItem.jsx
@@ -1,8 +1,10 @@
 import React, { memo } from 'react';
 import styles from './videoItem.module.css';
+import unescape, { all } from 'unescape';
 
 const VideoItem = memo(({ video, video : {snippet}, onVideoClick, display}) => {
     const displayType = display === 'list' ? styles.list : styles.grid;
+
     return (
         <li className={`${styles.container} ${displayType}`} onClick={() => onVideoClick(video)}>
             <div className={styles.video}>
@@ -12,7 +14,7 @@ const VideoItem = memo(({ video, video : {snippet}, onVideoClick, display}) => {
                     alt="video thumbnail" 
                 />
                 <div className={styles.metadata}>
-                    <p className={styles.title}>{snippet.title}</p>
+                    <p className={styles.title}>{snippet.title = unescape(snippet.title)}</p>
                     <p className={styles.channel}>{snippet.channelTitle}</p>
                 </div>
             </div>
